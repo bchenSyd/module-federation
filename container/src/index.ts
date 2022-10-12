bfe24fda40989a69739191213967878dfb466546
Bo Chen <bo.chen@revenue.nsw.gov.au>
2022-10-12 16:10:32 +1100

    update

--- container/src/index.ts
+++ container/src/index.ts
@@ -1,1 +1,5 @@
-import('./bootstrap')
+// need to do it this way at applicaiton entry point
+// to make module load NOT eager;
+// otherwise we won't be able to put modules to shared scope
+// due to the 'Shared module is not available for eager consumption' error (which is self-explaining in error name itself)
+import('./bootstrap') ; // async call; make **all** modules  NOT eager loaded (i.e. lazy loaded)
